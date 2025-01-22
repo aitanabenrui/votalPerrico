e4794a9cd1e3a3a43f6636e609eb500d4eda1cae
Aitana <aitanabenrui2001@gmail.com>
2025-01-22 12:09:40 +0100

    style: add grid

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const perricosArray = ['https://lh5.googleusercontent.com/proxy/FkxxQHWFM7oVQre0EOcbaXyOBNM6RMwnyE3HCJ7Ii9rJZMMWV1bqHsW6e74eKGkAfdXj8a0xdGGpB-pPKZnWi-8O7kiUUIFK6WAyFBjaxueNFcn0oBE_-R0yU8wfeWq-Kg'];
+const perricosArray = [];
+
 console.log(perricosArray);
 
 const dogList = document.querySelector('#dog-list'); //modifica por id el div con id dog-list
 
 function renderPerricoArray(){
 
-    perricosArray.forEach((dogImage)=>{
+    dogList.innerHTML = ''; // Limpiar el contenido de la lista de perros
+
+    perricosArray.forEach((dogImage, index)=>{
         const htmlAdd =
         `<div class="dogCard" >
             <img class="image" id="perro" alt="imagen de perro" src="${dogImage}">
-            <div id="cardVote" > 
+            <div class="cardVote" > 
+                <div class="voteCard">
                     <p>😍</p>
                     <button>Precioso</button> 
                 </div>
@@ -20,17 +24,43 @@ function renderPerricoArray(){
             </div>
         </div>`;
 
+        console.log('innerHtml posición', index, dogList.innerHTML);
+
         dogList.innerHTML += htmlAdd;
-    })
+    });
 }
+
 console.log(dogList);
 
+// Función para agregar un perrico aleatorio al array
 const addPerrico = async ()=>{
-    const perricoImg = await getRandomDogImage();
+    const perricoImg = await getRandomDogImage(); //la función getRandomDogImage se declara en el archivo api.js
+    console.log(perricoImg);
     perricosArray.push(perricoImg);
-    renderPerricoArray();
-}
+    renderPerricoArray(); // Re-renderizar la lista con la nueva imagen
+};
 
-document.querySelector('#add-1-perrico').addEventListener('click', function(){
+// Función para agregar 5 perricos: definimos una función asíncrona con un for que no dejará de ejecutarse hasta que i > 5, es decir que pushee al array perricosArrar 5 imágenes random
+const add5Perricos = async () => {
+    for (let i = 0; i < 5; i++) {
+        const perricoImg = await getRandomDogImage();
+        perricosArray.push(perricoImg);
+    }
+    renderPerricoArray(); // Re-renderizar la lista con las nuevas imágenes
+};
+
+// Renderizar los perricos al cargar la página
+// renderPerricoArray(); //esto hace que la página tenga de primeras imagenes con perritos y no queremos eso, queremos que la página no tenga ningún perrito 
+
+// Agregar evento al botón "Añadir 1 perrico más"
+document.querySelector('#add-1-perrico').addEventListener('click', function(event){
+    event.preventDefault(); // Evitar el envío del formulario
     addPerrico();
 });
+
+// Agregar evento al botón "Añadir 5 perricos más"
+document.querySelector('#add-5-perrico').addEventListener('click', function(event){
+    event.preventDefault(); // Evitar el envío del formulario
+    add5Perricos();
+});
+
