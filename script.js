47753ad351c6150f7aea20a7189d67cf5b5f98a3
Aitana <aitanabenrui2001@gmail.com>
2025-01-26 20:17:18 +0100

    feat: vote button fixed

--- script.js
+++ script.js
@@ -1,54 +1,66 @@
 const perricosArray = [];
 //const votesArray = [];
 
+
 console.log(perricosArray);
+
+// addPerrico();
 
 const dogList = document.querySelector('#dog-list'); //modifica por id el div con id dog-list
 
+function addSocialListeners(){
+ // Función para los votos positivos
+document.querySelectorAll('.like').forEach((buttonNode) => {
+    buttonNode.addEventListener('click', function () {
+        console.log('me gusta clickado');
+        const hermanico = buttonNode.previousElementSibling;
+        const likeCountNode = hermanico.querySelector('.like-count');
+        likeCountNode.innerText = Number(likeCountNode.innerText) + 1
+    });
+});
+
+//Función para los votos negarivos
+document.querySelectorAll('.dislike').forEach((buttonNode) => {
+    buttonNode.addEventListener('click', function () {
+        console.log('me gusta clickado');
+        const hermanico = buttonNode.previousElementSibling;
+        const dislikeCountNode = hermanico.querySelector('.dislike-count');
+        dislikeCountNode.innerText = Number(dislikeCountNode.innerText) + 1
+    });
+});
+}
+
+function renderPerrico(dogImage){
+    const htmlAdd =
+    `<div class="dogCard" >
+        <img class="image" id="perro" alt="imagen de perro" src="${dogImage}">
+        <div class="cardVote" > 
+            <div class="voteCard">
+                <p>😍 <span class="like-count"></span></p>
+                <button class="like">Precioso</button> 
+            </div>
+            <div class="voteCard">
+                <p>🤢 <span class="dislike-count"></span></p>
+                <button class="dislike">Feísimo</button>
+            </div>
+        </div>
+    </div>`;
+
+    document.querySelector('#dog-list').innerHTML += htmlAdd;
+
+}
+
 
 function renderPerricoArray(){
-
     dogList.innerHTML = ''; // Limpiar el contenido de la lista de perros
 
-    perricosArray.forEach((dogImage, index)=>{
+    perricosArray.forEach((dogImage)=>{
         //const votes = votesArray[index] || {precioso:0, feisimo:0}; //inicializamos votos si no existen
-        const htmlAdd =
-        `<div class="dogCard" >
-            <img class="image" id="perro" alt="imagen de perro" src="${dogImage}">
-            <div class="cardVote" > 
-                <div class="voteCard">
-                    <p>😍 <span class="like-count"></span></p>
-                    <button class="like">Precioso</button> 
-                </div>
-                <div class="voteCard">
-                    <p>🤢 <span class="dislike-count"></span></p>
-                    <button class="dislike">Feísimo</button>
-                </div>
-            </div>
-        </div>`;
-
+        renderPerrico(dogImage);
         console.log('innerHtml posición', index, dogList.innerHTML);
-
-        dogList.innerHTML += htmlAdd;
     });
 
-    // Función para los votos positivos
-    const likeCountNodes = document.querySelectorAll('.like-count')
-    document.querySelectorAll('.like').forEach((buttonNode, index) => {
-        buttonNode.addEventListener('click', function () {
-            console.log('me gusta clickado');
-            likeCountNodes[index].innerText = Number(likeCountNodes[index].innerText) + 1
-        });
-    });
-
-    //Función para los votos negarivos
-    const dislikeCountNodes = document.querySelectorAll('.dislike-count')
-    document.querySelectorAll('.dislike').forEach((buttonNode, index) => {
-        buttonNode.addEventListener('click', function () {
-            console.log('me gusta clickado');
-            dislikeCountNodes[index].innerText = Number(dislikeCountNodes[index].innerText) + 1
-        });
-    });
+    addSocialListeners();
 }
 
 console.log(dogList);
@@ -58,16 +70,15 @@ const addPerrico = async ()=>{
     const perricoImg = await getRandomDogImage(); //la función getRandomDogImage se declara en el archivo api.js
     console.log(perricoImg);
     perricosArray.push(perricoImg);
-    renderPerricoArray(); // Re-renderizar la lista con la nueva imagen
+    renderPerrico(perricoImg); // Re-renderizar la lista con la nueva imagen
+    addSocialListeners();
 };
 
 // Función para agregar 5 perricos: definimos una función asíncrona con un for que no dejará de ejecutarse hasta que i > 5, es decir que pushee al array perricosArrar 5 imágenes random
 const add5Perricos = async () => {
     for (let i = 0; i < 5; i++) {
-        const perricoImg = await getRandomDogImage();
-        perricosArray.push(perricoImg);
+        addPerrico(); 
     }
-    renderPerricoArray(); // Re-renderizar la lista con las nuevas imágenes
 };
 
 // Renderizar los perricos al cargar la página
